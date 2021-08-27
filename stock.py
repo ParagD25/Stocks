67314834eab4543f49e3d51dabfe6d2c90cd2dfe
Parag <paragdurafe@outlook.com>
2021-08-27 22:50:09 +0530

    Initial commit

---
+++ stock.py
@@ -0,0 +1,6 @@
+import datetime
+from pandas_datareader import data
+# stock_start=datetime.datetime(2017,7,25)
+# stock_last=datetime.datetime(2017,8,25)
+# df=data.DataReader(name="GOOG",data_source="yahoo",start=stock_start,end=stock_last)
+# print(df)
