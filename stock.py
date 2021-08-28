a145719439035abc3decc0edfd0568aa3c0622b9
Parag <paragdurafe@outlook.com>
2021-08-28 23:50:36 +0530

    Extracted stocks

--- stock.py
+++ stock.py
@@ -1,6 +1,8 @@
 import datetime
 from pandas_datareader import data
-# stock_start=datetime.datetime(2017,7,25)
-# stock_last=datetime.datetime(2017,8,25)
-# df=data.DataReader(name="GOOG",data_source="yahoo",start=stock_start,end=stock_last)
-# print(df)
+from bokeh.plotting import show,figure,output_file
+
+stock_first=datetime.datetime(2021,7,1)
+stock_last=datetime.datetime.now()
+df=data.DataReader(name="GOOG",data_source="yahoo",start=stock_first,end=stock_last)
+print(df)
